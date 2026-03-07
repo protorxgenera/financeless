c03c5d6c8b4e10c917af3354a86cb0a768974a67
protorxgenera <109683499+protorxgenera@users.noreply.github.com>
2026-03-07 20:52:26 +0200

    feature: add router skeleton

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,24 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {Dashboard} from './pages/dashboard/dashboard';
+import {Transactions} from './pages/transactions/transactions';
+import {Analytics} from './pages/analytics/analytics';
+import {Settings} from './pages/settings/settings';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Dashboard
+    },
+    {
+        path: 'transactions',
+        component: Transactions
+    },
+    {
+        path: 'analytics',
+        component: Analytics
+    },
+    {
+        path: 'settings',
+        component: Settings
+    }
+];
