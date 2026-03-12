f5cef42c374ce0ae6f61382780c753c5a372ed92
protorxgenera <109683499+protorxgenera@users.noreply.github.com>
2026-03-12 11:56:16 +0200

    fix: refresh button on table date picker gets disabled on click

--- frontend/src/app/pages/transactions/transaction-table/components/calendar-date-picker-range.ts
+++ frontend/src/app/pages/transactions/transaction-table/components/calendar-date-picker-range.ts
@@ -1,35 +1,38 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { HlmButton } from '@spartan-ng/helm/button';
-import { HlmDateRangePicker } from '@spartan-ng/helm/date-picker';
-import { HlmLabel } from '@spartan-ng/helm/label';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {HlmButton} from '@spartan-ng/helm/button';
+import {HlmDateRangePicker} from '@spartan-ng/helm/date-picker';
+import {NgIcon, provideIcons} from '@ng-icons/core';
+import {lucideRefreshCw} from '@ng-icons/lucide';
+import {HlmIcon} from '@spartan-ng/helm/icon';
 
 @Component({
     selector: 'cal-date-picker-range',
-    imports: [HlmDateRangePicker, ReactiveFormsModule, HlmLabel, HlmButton],
+    imports: [HlmDateRangePicker, ReactiveFormsModule, HlmButton, HlmIcon, NgIcon],
+    providers: [provideIcons({lucideRefreshCw})],
     changeDetection: ChangeDetectionStrategy.OnPush,
     host: {
         class: 'flex justify-center bg-surface overflow-auto',
     },
     template: `
-		<form [formGroup]="form" (ngSubmit)="submit()" class="space-y-8">
-			<div class="flex flex-row gap-2">
-				<hlm-date-range-picker
-					buttonId="dateRange"
-					[min]="minDate"
-					[max]="maxDate"
-					formControlName="range"
-					[autoCloseOnEndSelection]="true"
+        <form [formGroup]="form" (ngSubmit)="submit()" class="space-y-8">
+            <div class="flex flex-row gap-2">
+                <hlm-date-range-picker
+                    buttonId="dateRange"
+                    [min]="minDate"
+                    [max]="maxDate"
+                    formControlName="range"
+                    [autoCloseOnEndSelection]="true"
                     class="h-8"
-				>
-					<span>Enter a date range</span>
-				</hlm-date-range-picker>
-                <button type="submit" hlmBtn class="h-8" [disabled]="form.invalid">Refresh</button>
-			</div>
+                >
+                    <span>Enter a date range</span>
+                </hlm-date-range-picker>
+                <button type="submit" hlmBtn class="h-8" [disabled]="form.invalid || form.pristine"><ng-icon hlm name="lucideRefreshCw" size="sm"/></button>
+            </div>
 
 
-		</form>
-	`,
+        </form>
+    `,
 })
 export class CalendarDatePickerRangeComponent {
     private readonly _formBuilder = inject(FormBuilder);
@@ -46,6 +49,7 @@ export class CalendarDatePickerRangeComponent {
 
     submit() {
         console.log(this.form.value); //TODO update this to query the backend with the user-provided range
+        this.form.get('range')?.markAsPristine();
     }
 
     constructor() {
